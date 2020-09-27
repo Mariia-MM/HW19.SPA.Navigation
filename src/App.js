434a193099ed188a80053c11bf7b44a92d8c53ca
Mariia <mytsko.mariia@gmail.com>
2020-09-27 22:27:16 +0200

    change browserrouter to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router,Switch,  Route, Redirect} from "react-router-dom";
+import {HashRouter as Router,Switch,  Route, Redirect} from "react-router-dom";
 import Homepage from "./components/HomePage/HomePage";
 import Photos from "./components/Photos/Photos";
 import Posts from "./components/Posts/Posts";
